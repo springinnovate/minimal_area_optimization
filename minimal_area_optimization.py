526c06911753e9c2ebd52eae3a8f112c6d1e7c06
Rich Sharp <richpsharp@gmail.com>
2021-08-09 18:46:48 -0700

    trying to do local optimization multigrid

--- minimal_area_optimization.py
+++ minimal_area_optimization.py
@@ -39,37 +39,134 @@ def _make_test_data(dir_path, n, m):
     return raster_path_list
 
 
-def _construct_optimization_problem(raster_path_list):
-    """Construct optimization problem from raster path list."""
-    raster_info = ecoshard.geoprocessing.get_raster_info(raster_path_list[0])
-    prob = pulp.LpProblem('NCP_area_problem', pulp.LpMinimize)
-    n_rows, n_cols = raster_info['raster_size']
-    variable_list = []
-    LOGGER.info('construct area list')
-    for row in range(n_rows):
-        for col in range(n_cols):
-            flat_index = row*n_cols+col
-            var = pulp.LpVariable(f'x{flat_index}', 0, 1)
-            variable_list.append(var)
-    for raster_path in raster_path_list:
-        #LOGGER.info(f'processing {raster_path}')
-        raster = gdal.OpenEx(raster_path)
-        array = raster.ReadAsArray()
-
-        ncp_area_value_list = []
-        for row in range(n_rows):
-            for col in range(n_cols):
-                flat_index = row*n_cols+col
-                ncp_area_value_list.append(
-                    variable_list[flat_index] * array[row, col])
-        prob += sum(ncp_area_value_list) >= 0.9 * numpy.sum(array), f'{os.path.basename(raster_path)}_ncp'
-    prob += sum(variable_list)
-    return prob
-
 def _callback(prob):
     LOGGER.debug(f'phase: {prob.phase}, status: {prob.status} {prob.message}')
 
-def _scipy_optimize(raster_path_list):
+
+def _sum_raster(raster_path):
+    """Return the non-nodata sum of the raster."""
+    raster_info = ecoshard.geoprocessing.get_raster_info(raster_path)
+    nodata = raster_info['nodata'][0]
+    running_sum = 0.0
+    for _, array in ecoshard.geoprocessing.iterblocks((raster_path, 1)):
+        if nodata is not None:
+            valid_mask = (array != nodata)
+        else:
+            valid_mask = numpy.ones(array.shape, dtype=bool)
+        running_sum += numpy.sum(array[valid_mask])
+    return running_sum
+
+
+def multigrid_optimize(
+        raster_path_list, min_proportion, target_raster_path,
+        win_xoff, win_yoff, win_xsize, win_ysize,
+        prop_tol=1e-12, grid_size=64,):
+    """Solve a multigrid optimization problem.
+
+    Args:
+        raster_path_list (list): list of rasters of equal shape to
+            use for value objective matching.
+        min_proportion (float): minimum proportion of rasters to optimize
+            for.
+        prop_tol (float): propotion of raster sum to use as tolerance.
+        grid_size (int): the size to subdivide the optimization problem on.
+
+    Return:
+        {
+            'objective_sum_list': [sum of selected objectives],
+            'proportion_list': [proportion of raster selected],
+            'area_list': [area of raster selected]
+        }
+    """
+    raster_info = ecoshard.geoprocessing.get_raster_info(raster_path_list[0])
+    sum_list = [_sum_raster(path) for path in raster_path_list]
+
+    n_cols, n_rows = raster_info['raster_size']
+
+    n_col_steps = int(numpy.ceil(win_xsize / grid_size))
+    n_row_steps = int(numpy.ceil(win_ysize / grid_size))
+    while n_col_steps > 1 and n_row_steps > 1:
+        LOGGER.debug(f'{n_col_steps}, {n_row_steps}')
+        x_stepsize = win_xsize // n_col_steps
+        y_stepsize = win_ysize // n_row_steps
+
+        c_vector = numpy.ones(n_col_steps*n_row_steps)
+        A_list = []
+        b_list = []
+
+        for raster_path in raster_path_list:
+            LOGGER.info(f'processing {raster_path}')
+            raster = gdal.OpenEx(raster_path)
+            band = raster.GetRasterBand(1)
+            nodata = band.GetNoDataValue()
+            a_constraint_list = []
+            tot_val = 0.0
+            var_index = 0
+            offset_list = []
+            for x_index in range(n_col_steps):
+                local_xoff = x_index * x_stepsize
+                if (x_index+1)*x_stepsize < n_cols:
+                    local_win_xsize = x_stepsize
+                else:
+                    local_win_xsize = n_cols - (x_index+1)*n_col_steps
+
+                for y_index in range(n_row_steps):
+                    local_yoff = y_index * y_stepsize
+                    if (y_index+1)*y_stepsize < n_rows:
+                        local_win_ysize = y_stepsize
+                    else:
+                        local_win_ysize = n_rows - (y_index+1)*y_stepsize
+
+                    tol = 1.0
+
+                    # load the subgrid
+                    offset_dict = {
+                        'xoff': local_xoff,
+                        'yoff': local_yoff,
+                        'win_xsize': local_win_xsize,
+                        'win_ysize': local_win_ysize,
+                    }
+                    offset_list.append(offset_dict)
+                    array = band.ReadAsArray(**offset_dict)
+                    if nodata is not None:
+                        valid_mask = array != nodata
+                    else:
+                        valid_mask = numpy.ones(array.shape, dtype=bool)
+                    # aggregate it up to a single value
+                    array_sum = numpy.sum(array[valid_mask])
+                    a_constraint_list.append(-array_sum)
+                    tot_val += array_sum
+            A_list.append(a_constraint_list)
+            b_list.append(-min_proportion*tot_val)
+
+        c_vector = numpy.ones(len(a_constraint_list))
+        res = scipy.optimize.linprog(
+            c_vector,
+            A_ub=A_list,
+            b_ub=b_list,
+            bounds=[0, 1],
+            options={'tol': tol, 'disp': True})
+        for local_offset, local_prop in zip(offset_list, res.x):
+            LOGGER.debug(res.x)
+            multigrid_optimize(
+                raster_path_list, min_proportion, target_raster_path,
+                local_offset['xoff'], local_offset['yoff'],
+                local_offset['win_xsize'], local_offset['win_ysize'],
+                prop_tol=1e-12, grid_size=64,)
+        # iterate over each solution of x here and solve that subproblem
+
+                #LOGGER.debug(
+                #    f'sampling offset for x{var_index}\n'
+                #    f'\t{xoff}, {yoff}, {win_xsize}, {win_ysize} ({xoff+win_xsize}, {yoff+win_ysize})')
+
+                #var_index += 1
+
+        grid_size *= 2
+        n_col_steps = int(numpy.ceil(n_cols / grid_size))
+        n_row_steps = int(numpy.ceil(n_rows / grid_size))
+
+
+def _scipy_optimize(raster_path_list, min_proportion):
     raster_info = ecoshard.geoprocessing.get_raster_info(raster_path_list[0])
     n_rows, n_cols = raster_info['raster_size']
     LOGGER.info('construct area list')
@@ -86,24 +183,18 @@ def _scipy_optimize(raster_path_list):
         A_list.append(-array.flatten())
         array_sum = numpy.sum(array)
         tot_val += array_sum
-        b_list.append(-0.9*array_sum)
-        tol = min(tol, 1e-6*array_sum)
+        b_list.append(-min_proportion*array_sum)
+        tol = min(tol, 1e-12*array_sum)
 
     LOGGER.debug('solving problem')
-    #print(c_vector)
-    A_matrix = numpy.array(A_list)
-    #print(A_matrix)
-    #print(A_matrix.shape)
-    #print(b_list)
     res = scipy.optimize.linprog(
         c_vector,
         A_ub=A_list,
         b_ub=b_list,
         bounds=[0, 1],
-        method='revised simplex',
-        #callback=_callback,
         options={'tol': tol, 'disp': True})
-    return res, tot_val
+    raster_results = [numpy.sum(A * res.x)/numpy.sum(A) for A in A_list]
+    return res, tot_val, raster_results
 
 def main():
     """Entry point."""
@@ -118,11 +209,26 @@ def main():
     #problem = _construct_optimization_problem(raster_path_list)
     #LOGGER.info('solve optimization problem')
     #problem.solve()
-    problem, tot_val = _scipy_optimize(raster_path_list)
-    LOGGER.debug(problem.fun)
-    LOGGER.debug(problem.fun/tot_val)
-    LOGGER.debug(sum(problem.x)/(n*n))
+    min_proportion = 0.9
 
+    #problem, tot_val, raster_results = _scipy_optimize(raster_path_list, min_proportion)
+
+    target_raster_path = 'result.tif'
+    multigrid_optimize(
+        raster_path_list, min_proportion, target_raster_path,
+        prop_tol=1e-12, grid_size=64)
+    return
+
+
+    LOGGER.debug(problem.fun)
+    LOGGER.debug(problem.fun/(n*n))
+    area_proportion = sum(problem.x)/(n*n)
+    result_proportion = problem.fun/tot_val
+    LOGGER.info(f'result\n\tarea_proportion: {area_proportion:.3f}\n\tsolution proportion for each raster: {raster_results}')
+    LOGGER.info(problem.x)
+    ecoshard.geoprocessing.numpy_array_to_raster(
+        (problem.x).reshape((n, n)), -1, (1, -1), (0, 0),
+        osr.SRS_WKT_WGS84_LAT_LONG, 'result.tif')
     #for v in problem.variables():
     #    print(v.name, "=", v.varValue)
 
